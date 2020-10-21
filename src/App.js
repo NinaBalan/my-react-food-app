8bb0657f07d9811a1fa67d716cb2196b6cdbffcc
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-10-21 21:17:13 +0300

    commit project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Axios from 'axios';
+import {v4 as uuidv4 } from 'uuid';
+
+
 import './App.css';
+import Recipe from './components/Recipe';
+import Alert from './components/Alert';
 
 function App() {
+
+  const [query,setQuery] = useState("");
+  const [recipes,setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const APP_ID = "32aaaad1";
+  const APP_KEY = "c28a7d269c8451d6ce354c7db0bf0261";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Food Searching App</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input 
+          type="text" 
+          placeholder="query" 
+          onChange={onChange} 
+          value={query} 
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] &&recipes.map(recipe => 
+        <Recipe key={uuidv4()} recipe={recipe} />)}
+      </div>
     </div>
   );
 }
